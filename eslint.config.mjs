10ac828498de31842f4f4c4e7acb811d8aad5a03
av <av@mcc.vniiem.ru>
2024-12-12 20:41:47 +0300

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,29 @@
-// @ts-check
-
-import eslint from "@eslint/js";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  tseslint.config(
-    {
-      ignores: ["*.config.*"],
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: {
+        module: "readonly", // Allow module globally
+        require: "readonly", // Allow require globally
+        process: "readonly", // Allow process globally
+        __dirname: "readonly", // Allow __dirname globally
+      },
     },
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-  ),
-  eslintConfigPrettier,
+  },
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["babel.config.js", "webpack.config.js"],
+    rules: {
+      "no-undef": "off", // Disable the no-undef rule for these files
+      "@typescript-eslint/no-require-imports": "off", // Allow require imports
+      "import/no-extraneous-dependencies": "off", // Disable any dependency rules
+    },
+  },
 ];
