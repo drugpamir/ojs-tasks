2e2d008e8e77742f6f440543790881d2c0d66ef0
drugpamir <toll_aa@mail.ru>
2024-12-13 19:56:04 +0300

    added some tests for calendarLocalStorage

---
+++ src/ts/api/calendarLocalStorage.test.ts
@@ -0,0 +1,41 @@
+import { generateTask } from "../utils/dataGenerator";
+import { Task } from "./calendarApi";
+import { CalendarLocalStorage } from "./calendarLocalStorage";
+
+describe("tasks local storage", () => {
+  let calendarLocalStorage: CalendarLocalStorage;
+
+  beforeEach(async () => {
+    calendarLocalStorage = new CalendarLocalStorage();
+  });
+
+  it("getTasks() returns empty task array if tasks not added before", async () => {
+    const tasks: Task[] = await calendarLocalStorage.getTasks();
+    expect(tasks.length).toBe(0);
+  });
+
+  //   it("getTasks() returns expected tasks count after them added", async () => {
+  //     const tasksPromises: Promise<Task>[] = [1, 2, 4, 6, 5, 12]
+  //       .map((num) => generateTask(num))
+  //       .map((task) => calendarLocalStorage.createTask(task));
+  //     const tasksToCreate = await Promise.all(tasksPromises);
+
+  //     let mockStorageGet = jest
+  //       .fn()
+  //       .mockImplementation((key: string) => JSON.stringify(tasksToCreate));
+  //     localStorage.getItem = mockStorageGet;
+
+  //     const storedTasks = await calendarLocalStorage.getTasks();
+  //     expect(storedTasks.length).toBe(tasksToCreate.length);
+  //   });
+
+  it("createTask() returns added task with same fields and generated id", async () => {
+    const newTask = generateTask(1);
+    const storedTask: Task = await calendarLocalStorage.createTask(newTask);
+    expect(storedTask.id).not.toBeUndefined();
+    expect(storedTask.createdAt).not.toBeUndefined();
+    expect(newTask.name).toBe(storedTask.name);
+    expect(newTask.categories).toBe(storedTask.categories);
+    expect(newTask.date).toBe(storedTask.date);
+  });
+});
--- src/ts/utils/dataGenerator.ts
+++ src/ts/utils/dataGenerator.ts
@@ -1,6 +1,16 @@
 // import { randomUUID } from "crypto";
 
+import { Task } from "../api/calendarApi";
+
 export function generateID(): string {
   return new Date().toString();
   //   return randomUUID();
+}
+
+export function generateTask(num: number): Omit<Task, "id" | "createdAt"> {
+  return {
+    name: `Task ${num}`,
+    categories: [`Category ${num}`],
+    date: new Date(2024, 12, 13, 1, 2, 3 + num),
+  };
 }
