6a0c8ab027a8c552dfa2215e0f22744c18dd049a
drugpamir <toll_aa@mail.ru>
2024-12-18 12:42:35 +0300

    default tasks for calendarDummyStorage

--- src/ts/api/calendarDummyStorage.ts
+++ src/ts/api/calendarDummyStorage.ts
@@ -2,46 +2,46 @@ import { generateID } from "../utils/dataGenerator";
 import { CalendarApi, Task } from "./calendarApi";
 
 export class CalendarDummyStorage implements CalendarApi {
-  #tasks: Task[] = [];
+  #tasks: Task[] = [
+    {
+      id: "1",
+      name: "Сходить в магазин",
+      categories: ["Покупки", "Прогулка"],
+      date: new Date(2024, 12, 15),
+      createdAt: new Date(2024, 12, 14),
+    },
+    {
+      id: "3",
+      name: "Помыть посуду",
+      categories: ["Домашнее"],
+      date: new Date(2024, 12, 14),
+      createdAt: new Date(2024, 12, 13),
+    },
+    {
+      id: "10",
+      name: "Заплатить за квартиру",
+      categories: ["Покупки", "Домашнее"],
+      date: new Date(2024, 12, 15),
+      createdAt: new Date(2024, 12, 15),
+    },
+    {
+      id: "11",
+      name: "Помочь тёще",
+      categories: ["Семья", "Поездка"],
+      date: new Date(2024, 12, 15),
+      createdAt: new Date(2024, 12, 16),
+    },
+    {
+      id: "13",
+      name: "Покормить кота",
+      categories: ["Семья", "Домашнее"],
+      date: new Date(2024, 12, 12),
+      createdAt: new Date(2024, 12, 14),
+    },
+  ];
 
   async getTasks(): Promise<Task[]> {
-    return [
-      {
-        id: "1",
-        name: "Сходить в магазин",
-        categories: ["Покупки", "Прогулка"],
-        date: new Date(2024, 12, 15),
-        createdAt: new Date(2024, 12, 14),
-      },
-      {
-        id: "3",
-        name: "Помыть посуду",
-        categories: ["Домашнее"],
-        date: new Date(2024, 12, 14),
-        createdAt: new Date(2024, 12, 13),
-      },
-      {
-        id: "10",
-        name: "Заплатить за квартиру",
-        categories: ["Покупки", "Домашнее"],
-        date: new Date(2024, 12, 15),
-        createdAt: new Date(2024, 12, 15),
-      },
-      {
-        id: "11",
-        name: "Помочь тёще",
-        categories: ["Семья", "Поездка"],
-        date: new Date(2024, 12, 15),
-        createdAt: new Date(2024, 12, 16),
-      },
-      {
-        id: "13",
-        name: "Покормить кота",
-        categories: ["Семья", "Домашнее"],
-        date: new Date(2024, 12, 12),
-        createdAt: new Date(2024, 12, 14),
-      },
-    ];
+    return this.#tasks;
   }
 
   async getTasksByDate(dateFrom: Date, dateTo: Date): Promise<Task[]> {
